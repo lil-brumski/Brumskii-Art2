230f685e51a94a59af7d243b0f7990f9a5c0cdd3
lil-brumski <davidbrume7@gmail.com>
2024-12-01 12:46:59 +0100

    Semi-completed the project.
    Completed BrumskiiArt::ArtTwo() function, created a smart pointer that calls BrumskiiArt::ArtOne() and BrumskiiArt::ArtTwo() repeatedly, updated project description in README.md

--- src/Brumskii-Art2.cpp
+++ src/Brumskii-Art2.cpp
@@ -36,7 +36,48 @@ namespace BrumskiiArt2{
         std::cout << "            ==     ==              \n";
         std::cout << "           ===     ===             \n";
         
-        std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1200));
+        #if defined(_WIN32) || defined(_WIN64)
+          std::system("cls");
+        #else
+          std::system("clear");
+        #endif
+        
+        return *this;
+    }
+    
+    Art& Art::ArtTwo(){
+        #if defined(_WIN32) || defined(_WIN64)
+          std::system("cls");
+        #else
+          std::system("clear");
+        #endif
+        
+        std::cout << "               ===                 \n";
+        std::cout << "            ===   ===              \n";
+        std::cout << "           ==       ==             \n";
+        std::cout << "           ==       ==             \n";
+        std::cout << "     = = = ==       == = = =       \n";
+        std::cout << "       ==   ==     ==   ==         \n";
+        std::cout << "        ==   ==   ==   ==          \n";
+        std::cout << "         ==   == ==   ==           \n";
+        std::cout << "          ==  =====  ==            \n";
+        std::cout << "            =========              \n";
+        std::cout << "               ===                 \n";
+        std::cout << "               ===                 \n";
+        std::cout << "               ===                 \n";
+        std::cout << "               ===                 \n";
+        std::cout << "               ===                 \n";
+        std::cout << "               ===                 \n";
+        std::cout << "             ==   ==               \n";
+        std::cout << "           ==       ==             \n";
+        std::cout << "          ==         ==            \n";
+        std::cout << "         ==           ==           \n";
+        std::cout << "        ==             ==          \n";
+        std::cout << "        ==             ==          \n";
+        std::cout << "      ===               ===        \n";
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(1200));
         #if defined(_WIN32) || defined(_WIN64)
           std::system("cls");
         #else
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <Brumskii-Art2/Brumskii-Art2.hpp>
 #include <memory>
 
-int main() {
+void Brumski(){
     auto brumskiArt = std::make_unique<BrumskiiArt2::Art>(1);
-    brumskiArt->ArtOne();
+    brumskiArt->ArtOne().ArtTwo();
+    brumskiArt->ArtOne().ArtTwo();
+    brumskiArt->ArtOne().ArtTwo();
+    brumskiArt->ArtOne().ArtTwo();
+    brumskiArt->ArtOne().ArtTwo();
+}
+
+int main() {
+    Brumski();
     return 0;
 }
